6d9681ef8b5ccb483c5481f9fc8fb7a34c152c5e
yiyun <58024602+yiyunrin@users.noreply.github.com>
2024-03-23 22:02:50 +0800

    Add files via upload

--- js/web.js
+++ js/web.js
@@ -1,29 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var audio = document.getElementById('myAudio');
-
-    document.addEventListener('click', function () {
-        audio.play();
-    });
-});
-
 // document.addEventListener('DOMContentLoaded', function () {
-//     var trailElements = [];
+//     var audio = document.getElementById('myAudio');
 
-//     document.addEventListener('mousemove', function (event) {
-//         var trail = document.createElement('div');
-//         trail.classList.add('trail');
-//         trail.style.left = (event.pageX - 5) + 'px';
-//         trail.style.top = (event.pageY - 5) + 'px';
-//         document.body.appendChild(trail);
-//         trailElements.push(trail);
-
-//         setTimeout(function () {
-//             document.body.removeChild(trail);
-//             trailElements.splice(trailElements.indexOf(trail), 1);
-//         }, 100);
+//     document.addEventListener('click', function () {
+//         audio.play();
 //     });
 // });
 
+document.addEventListener('DOMContentLoaded', function () {
+    var audio = document.getElementById('myAudio');
 
+    document.addEventListener('click', function (event) {
+        if (event.target.tagName.toLowerCase() !== 'a') {
+            audio.play();
+        }
+    });
+});
 
 
